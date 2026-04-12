b8f2dabe0ca759ae1e05faf0b0e3ae5e886d7855
Barae <93267708+baraeerriyachy@users.noreply.github.com>
2026-04-12 18:55:44 +0200

    Add files via upload

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+-- ver: vertical lines, hor: horizontal characters
+-- Default is usually ver:3, hor:6. Setting ver:1 makes it as slow as possible.
+vim.opt.mousescroll = "ver:1,hor:1"
